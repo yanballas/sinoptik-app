9a742f4c081fa65841c080c918e614d216bc02a4
v.serbin <yanballas@yandex.ru>
2024-04-29 02:27:09 +0300

    add gh-pages and change name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex">
       <h1 className="text-lg inline-block uppercase font-bold content-end lg:text-2xl">
-        sinoptik app
+        sinoptik-app
       </h1>
       <img className="w-12 lg:w-16" src={Logo} alt="logo" />
     </header>
