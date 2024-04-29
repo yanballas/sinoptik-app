b77e33adc616b5e6bb6d2553771c311c7824441d
v.serbin <yanballas@yandex.ru>
2024-04-30 02:18:59 +0300

    add localstorage && searchitems

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { INITIAL_STATE } from "../helpers/INITIAL_STATE";
 import Layout from "./Layout";
 import Search from "./Search";
 
 export default function Main() {
-  const [value, setValue] = useState("");
-  const [items, setItems] = useState("");
+  const [searchValue, setSearchValue] = useState(
+    JSON.parse(localStorage.getItem("city")) || ""
+  );
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("cities")) || INITIAL_STATE
+  );
 
-  const handleSearchButton = () => {
-    console.log(value);
-  };
+  useEffect(() => {
+    console.log(items);
+  }, [items]);
 
   return (
     <main className="grow flex flex-col gap-8 min-h-0">
       <Search
-        value={value}
-        setValue={setValue}
-        handleSearchButton={handleSearchButton}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        items={items}
+        setItems={setItems}
       ></Search>
-      <Layout></Layout>
+      <Layout items={items} setItems={setItems}></Layout>
     </main>
   );
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,57 @@
 import Container from "./Container";
 import Button from "./Button";
 import Input from "./Input";
+import { API_Key, BASE_URL } from "../helpers/APIKEY";
+import { useEffect } from "react";
 
 export default function Search(props) {
-  const {
-    value = String,
-    setValue,
-    handleSearchButton = Function.prototype,
-  } = props;
+  const { searchValue = String, setSearchValue, items = [], setItems, ...prop } = props;
+
+  const saveLocalStorage = (key, elements) => {
+    localStorage.setItem(key, elements);
+  };
+
+  const sortItems = (elements) => {
+    return elements.sort((a, b) => {
+      if (a?.location?.name.toLowerCase() < b?.location?.name.toLowerCase())
+        return -1;
+      if (a?.location?.name.toLowerCase() > b?.location?.name.toLowerCase())
+        return 1;
+      return 0;
+    });
+  };
+
+  const searchItems = async (value = "") => {
+    const city = value.trim().toLowerCase();
+    try {
+      await fetch(`${BASE_URL}/current.json?key=${API_Key}&q=${city}&aqi=no`)
+        .then((response) => response.json())
+        .then((data) => setItems((state) => sortItems([...state, data])));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleSearchButton = () => {
+    searchItems(searchValue);
+  };
+  
+  useEffect(() => {
+    saveLocalStorage("cities", JSON.stringify(items));
+  }, [items]);
+
+  useEffect(() => {
+    saveLocalStorage('city', JSON.stringify(searchValue))
+  }, [searchValue])
 
   return (
     <Container>
       <div className="flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between lg:gap-0">
-        <Input value={value} setValue={setValue} name={'search'}></Input>
+        <Input
+          value={searchValue}
+          setValue={setSearchValue}
+          name={"search"}
+        ></Input>
         <Button
           className={"text-md bg-primary-200 hover:text-light lg:text-lg"}
           text={"get weather information"}
